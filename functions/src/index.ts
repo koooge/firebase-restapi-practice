8c0e3ab771d6c2c9b9ebb37d1aef49e338169d5f
koooge <koooooge@gmail.com>
2019-09-08 01:24:09 +0900

    function: Create makeUppercase()
    Signed-off-by: koooge <koooooge@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as functions from 'firebase-functions';
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+// https://us-central1-koooge-firebase-restapi.cloudfunctions.net/addMessage?text=uppercaseme
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
 exports.addMessage = functions.https.onRequest(async (req, res) => {
@@ -13,3 +14,18 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
   res.redirect(303, snapshot.ref.toString());
 });
+
+// https://us-central1-koooge-firebase-restapi.cloudfunctions.net/addMessage?text=uppercasemetoo
+// Listens for new messages added to /messages/:pushId/original and creates an
+// uppercase version of the message to /messages/:pushId/uppercase
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onCreate((snapshot, context) => {
+      // Grab the current value of what was written to the Realtime Database.
+      const original = snapshot.val();
+      console.log('Uppercasing', context.params.pushId, original);
+      const uppercase = original.toUpperCase();
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to the Firebase Realtime Database.
+      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+      return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });index.js
